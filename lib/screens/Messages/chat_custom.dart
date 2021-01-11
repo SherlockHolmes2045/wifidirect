fba3d5f0151f99786ef8c6c848eabe8014de19d3
SherlockHolmes2045 <lemovou@gmail.com>
2021-01-11 18:32:22 +0100

    added audio player

--- lib/screens/Messages/chat_custom.dart
+++ lib/screens/Messages/chat_custom.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'dart:convert';
+import 'package:audioplayer/audioplayer.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -14,6 +15,15 @@ import 'package:siuu_tchat/utils/message_type.dart';
 import 'dart:io' as Io;
 import 'package:file/file.dart';
 import 'package:file/local.dart';
+import 'dart:typed_data';
+
+
+enum PlayerState { stopped, playing, paused }
+
+typedef void OnError(Exception exception);
+
+const kUrl =
+    "https://www.mediacollege.com/downloads/sound-effects/nature/forest/rainforest-ambient.mp3";
 
 class Chat extends StatefulWidget {
   final String name;
@@ -28,10 +38,38 @@ class Chat extends StatefulWidget {
 }
 
 class _ChatState extends State<Chat> {
+
+  String audioText = "Maintenez pour enregister";
+  bool recordMode = false;
+  AudioPlayer audioPlugin = AudioPlayer();
   List<Map<String, dynamic>> chats = new List<Map<String, dynamic>>();
   FlutterAudioRecorder _recorder;
   Recording _current;
   RecordingStatus _currentStatus = RecordingStatus.Unset;
+
+  Duration duration;
+  Duration position;
+
+  AudioPlayer audioPlayer;
+
+  String localFilePath;
+
+  PlayerState playerState = PlayerState.stopped;
+
+  get isPlaying => playerState == PlayerState.playing;
+  get isPaused => playerState == PlayerState.paused;
+
+  get durationText =>
+      duration != null ? duration.toString().split('.').first : '';
+
+  get positionText =>
+      position != null ? position.toString().split('.').first : '';
+
+  bool isMuted = false;
+
+  StreamSubscription _positionSubscription;
+  StreamSubscription _audioPlayerStateSubscription;
+
 
   static const platform = const MethodChannel('samples.flutter.dev/battery');
   static const _channel_message =
@@ -87,6 +125,88 @@ class _ChatState extends State<Chat> {
     );
   }
 
+  Widget _buildPlayer() => Container(
+    padding: EdgeInsets.all(16.0),
+    child: Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Row(mainAxisSize: MainAxisSize.min, children: [
+          IconButton(
+            onPressed: isPlaying ? null : () => play(),
+            iconSize: 64.0,
+            icon: Icon(Icons.play_arrow),
+            color: Colors.cyan,
+          ),
+          IconButton(
+            onPressed: isPlaying ? () => pause() : null,
+            iconSize: 64.0,
+            icon: Icon(Icons.pause),
+            color: Colors.cyan,
+          ),
+          IconButton(
+            onPressed: isPlaying || isPaused ? () => stop() : null,
+            iconSize: 64.0,
+            icon: Icon(Icons.stop),
+            color: Colors.cyan,
+          ),
+        ]),
+        if (duration != null)
+          Slider(
+              value: position?.inMilliseconds?.toDouble() ?? 0.0,
+              onChanged: (double value) {
+                return audioPlayer.seek((value / 1000).roundToDouble());
+              },
+              min: 0.0,
+              max: duration.inMilliseconds.toDouble()),
+        if (position != null) _buildMuteButtons(),
+        if (position != null) _buildProgressView()
+      ],
+    ),
+  );
+
+  Row _buildProgressView() => Row(mainAxisSize: MainAxisSize.min, children: [
+    Padding(
+      padding: EdgeInsets.all(12.0),
+      child: CircularProgressIndicator(
+        value: position != null && position.inMilliseconds > 0
+            ? (position?.inMilliseconds?.toDouble() ?? 0.0) /
+            (duration?.inMilliseconds?.toDouble() ?? 0.0)
+            : 0.0,
+        valueColor: AlwaysStoppedAnimation(Colors.cyan),
+        backgroundColor: Colors.grey.shade400,
+      ),
+    ),
+    Text(
+      position != null
+          ? "${positionText ?? ''} / ${durationText ?? ''}"
+          : duration != null ? durationText : '',
+      style: TextStyle(fontSize: 24.0),
+    )
+  ]);
+
+  Row _buildMuteButtons() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        if (!isMuted)
+          FlatButton.icon(
+            onPressed: () => mute(true),
+            icon: Icon(
+              Icons.headset_off,
+              color: Colors.cyan,
+            ),
+            label: Text('Mute', style: TextStyle(color: Colors.cyan)),
+          ),
+        if (isMuted)
+          FlatButton.icon(
+            onPressed: () => mute(false),
+            icon: Icon(Icons.headset, color: Colors.cyan),
+            label: Text('Unmute', style: TextStyle(color: Colors.cyan)),
+          ),
+      ],
+    );
+  }
+
   addMessage(Status status, {String path = ""}) async {
     if (Status.TEXT == status) {
       if (messageEditingController.text.isNotEmpty) {
@@ -124,6 +244,89 @@ class _ChatState extends State<Chat> {
         messageEditingController.text = "";
       });
     }
+  }
+  void initAudioPlayer() {
+    audioPlayer = AudioPlayer();
+    _positionSubscription = audioPlayer.onAudioPositionChanged
+        .listen((p) => setState(() => position = p));
+    _audioPlayerStateSubscription =
+        audioPlayer.onPlayerStateChanged.listen((s) {
+          if (s == AudioPlayerState.PLAYING) {
+            setState(() => duration = audioPlayer.duration);
+          } else if (s == AudioPlayerState.STOPPED) {
+            onComplete();
+            setState(() {
+              position = duration;
+            });
+          }
+        }, onError: (msg) {
+          setState(() {
+            playerState = PlayerState.stopped;
+            duration = Duration(seconds: 0);
+            position = Duration(seconds: 0);
+          });
+        });
+  }
+
+  Future play() async {
+    await audioPlayer.play(kUrl);
+    setState(() {
+      playerState = PlayerState.playing;
+    });
+  }
+
+  Future _playLocal() async {
+    await audioPlayer.play(localFilePath, isLocal: true);
+    setState(() => playerState = PlayerState.playing);
+  }
+
+  Future pause() async {
+    await audioPlayer.pause();
+    setState(() => playerState = PlayerState.paused);
+  }
+
+  Future stop() async {
+    await audioPlayer.stop();
+    setState(() {
+      playerState = PlayerState.stopped;
+      position = Duration();
+    });
+  }
+
+  Future mute(bool muted) async {
+    await audioPlayer.mute(muted);
+    setState(() {
+      isMuted = muted;
+    });
+  }
+
+  void onComplete() {
+    setState(() => playerState = PlayerState.stopped);
+  }
+
+  Future<Uint8List> _loadFileBytes(String url, {OnError onError}) async {
+    Uint8List bytes;
+    try {
+      bytes = await readBytes(url);
+    } on ClientException {
+      rethrow;
+    }
+    return bytes;
+  }
+
+  Future _loadFile() async {
+    final bytes = await _loadFileBytes(kUrl,
+        onError: (Exception exception) =>
+            print('_loadFile => exception $exception'));
+
+    final dir = await getApplicationDocumentsDirectory();
+    final file = Io.File('${dir.path}/audio.mp3');
+
+    await file.writeAsBytes(bytes);
+    if (await file.exists())
+      setState(() {
+        localFilePath = file.path;
+      });
   }
 
   pushReceivedMessage(String message, String type) {
@@ -166,14 +369,18 @@ class _ChatState extends State<Chat> {
   void initState() {
     super.initState();
     _init();
+    initAudioPlayer();
     _enableTimer();
   }
 
   @override
   void dispose() {
-    // TODO: implement dispose
-    super.dispose();
+    _positionSubscription.cancel();
+    _audioPlayerStateSubscription.cancel();
+    audioPlayer.stop();
     _disableTimer();
+    super.dispose();
+
   }
 
   @override
@@ -292,9 +499,10 @@ class _ChatState extends State<Chat> {
                           ),
                         ),
                         GestureDetector(
-                          onTap: () async {
-                            /*File result = await FilePicker.getFile(type: FileType.image);
-                                  addMessage(Status.IMAGE, path: result.path);*/
+                          onTap: ()  {
+                            setState(() {
+                              recordMode = true;
+                            });
                           },
                           child: Container(
                             height: 40,
@@ -316,36 +524,42 @@ class _ChatState extends State<Chat> {
                     ),
                   ),
                 ),
-                GestureDetector(
+                recordMode ? GestureDetector(
                   onLongPress: (){
                     _start();
                   },
                   onLongPressEnd: (longPressEndDetails){
                     _stop();
                   },
-                  child: Container(
-                    alignment: Alignment.bottomCenter,
-                    width: MediaQuery.of(context).size.width,
-                    height: MediaQuery.of(context).size.height / 7,
-                    child: Container(
-                      alignment: Alignment.center,
-                      child: CircleAvatar(
-                        radius: 35.0,
-                        child: Icon(
-                            Icons.mic,
-                          size: 30.0,
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding:  EdgeInsets.only(top: MediaQuery.of(context).size.height/20),
+                        child: Text(
+                          audioText,
+                          style: TextStyle(
+                            fontSize: 20.0
+                          ),
                         ),
                       ),
-                    )
+                      Container(
+                        alignment: Alignment.bottomCenter,
+                        width: MediaQuery.of(context).size.width,
+                        height: MediaQuery.of(context).size.height / 7,
+                        child: Container(
+                          alignment: Alignment.center,
+                          child: CircleAvatar(
+                            radius: 35.0,
+                            child: Icon(
+                                Icons.mic,
+                              size: 30.0,
+                            ),
+                          ),
+                        )
+                      ),
+                    ],
                   ),
-                )
-                /*Container(
-                  alignment: Alignment.bottomCenter,
-                  width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height / 7,
-                  child: AudioRecoder(
-                  ),
-                ),*/
+                ) : Container()
               ],
             ),
           ],
@@ -402,6 +616,7 @@ class _ChatState extends State<Chat> {
       var recording = await _recorder.current(channel: 0);
       setState(() {
         _current = recording;
+        audioText = "Enregistre...";
       });
 
       const tick = const Duration(milliseconds: 50);
@@ -441,6 +656,8 @@ class _ChatState extends State<Chat> {
     setState(() {
       _current = result;
       _currentStatus = _current.status;
+      audioText = "Maintenez pour enregister";
+      recordMode = false;
     });
   }
 
